d8ec60ba632456d37efedaef1be84719972e9058
jafb321 <jafb321@gmail.com>
2022-01-13 17:48:52 -0700

    feat: Use Layout on pages

--- src/pages/downloads/[id].tsx
+++ src/pages/downloads/[id].tsx
@@ -1,11 +1,29 @@
 import React from 'react'
 import { useRouter } from 'next/router'
+import { DownloadCard } from '../../components/downloads/DownloadCard';
+import Layout from '../../components/common/Layout';
 
 const DownloadPage = () => {
+
+    const router = useRouter();
+
+    const {id} = router.query;
+    
+    const data = {
+		title: 'Descargar Filmora X',
+		content: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Facilis quasi voluptates laudantium accusamus temporibus aliquam rem dolore earum similique, vero facere delectus cumque consequuntur at ad eligendi! Quis, sint velit!',
+		url: 'https://www.google.com'
+	}
+
+	const {content, title, url} = data;
+
     return (
-        <div>
+        <Layout>
+            <DownloadCard url={url} content={content} title={title} />
+            <DownloadCard url={url} content={content} title={title} />
+            <DownloadCard url={url} content={content} title={title} />
             
-        </div>
+        </Layout>
     )
 }
 
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,28 +1,12 @@
 import Head from 'next/head'
-import Image from 'next/image'
 import { Header } from '../components/common/Header'
+import Layout from '../components/common/Layout';
 import { DownloadCard } from '../components/downloads/DownloadCard';
 
 export default function Home() {
 
-    const logo = "Darko dev";
-
-	const data = {
-		title: 'Descargar Filmora X',
-		content: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Facilis quasi voluptates laudantium accusamus temporibus aliquam rem dolore earum similique, vero facere delectus cumque consequuntur at ad eligendi! Quis, sint velit!',
-		url: 'https://www.google.com'
-	}
-
-	const {content, title, url} = data;
-
   	return (
-		<div className="bg-gray-800 h-screen w-screen">
-			<Header title={logo} />
-			
-			<main className="flex container justify-center m-auto">
-				<DownloadCard url={url} content={content} title={title} />
-			</main>
-
-		</div>
+		<Layout>
+		</Layout>
 	);
 }
