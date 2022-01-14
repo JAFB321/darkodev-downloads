0820ac71adaf355e1eb583e3655c0835300a500d
jafb321 <jafb321@gmail.com>
2022-01-13 17:48:32 -0700

    feat: Layout component

---
+++ src/components/common/Layout.tsx
@@ -0,0 +1,22 @@
+import React, {ReactNode} from 'react'
+import { Header } from './Header'
+
+interface LayoutProps {
+    children?: ReactNode
+}
+
+const Layout = ({children}: LayoutProps) => {
+    return (
+        <>
+            <div className="bg-gray-800 h-screen w-screen">
+                <Header />
+
+                <main className="flex container flex-col items-center m-auto">
+                    {children}
+                </main>
+            </div>
+        </>
+    )
+}
+
+export default Layout
