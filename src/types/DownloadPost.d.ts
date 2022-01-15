3f41d360fb29677cb5da7618e462cae55696bd22
JAFB321 <jafb321@gmail.com>
2022-01-14 18:39:43 -0700

    feat: Download Post type

---
+++ src/types/DownloadPost.d.ts
@@ -0,0 +1,13 @@
+export default interface DownloadPost {
+    id: number | string,
+    title: string,
+	url: string,
+	shortened_url?: string,
+	shortened_ads_url?: string,
+	content?: string,
+	images?: string[],
+    url_button: boolean,
+    shortened_url_button: boolean,
+    shortened_ads_button: boolean,
+	delay_time?: number,
+}
